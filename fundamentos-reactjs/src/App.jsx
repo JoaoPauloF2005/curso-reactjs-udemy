ab19cd73f3a8a7203e071958a66c5870e15f297a
joao <hypergamerfrr@gmail.com>
2023-08-12 14:05:18 -0300

    att

--- fundamentos-reactjs/src/App.jsx
+++ fundamentos-reactjs/src/App.jsx
@@ -1,21 +1,43 @@
+import "./App.css";
 import React from "react";
 
-import Primeiro from './components/basicos/Primeiro';
-import ComParametro from './components/basicos/ComParametro';
-import Fragmento from './components/basicos/Fragmento';
+import FamiliaMembro from "./components/basicos/FamiliaMembro";
+import Familia from "./components/basicos/Familia";
+import Card from "./components/layout/Card";
+import Aleatorio from "./components/basicos/Aleatorio";
+import Primeiro from "./components/basicos/Primeiro";
+import ComParametro from "./components/basicos/ComParametro";
+import Fragmento from "./components/basicos/Fragmento";
 
 export default () => (
+  <div className="App">
+    <h1>Fundamentos do React</h1>
 
-    <div id="app">
-        <h1>Fundamentos do React 2 (Arrow _)</h1>
+    <div className="Cards">
+      <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
+        <Familia sobrenome="Silva">
+          <FamiliaMembro nome="Pedro" />
+          <FamiliaMembro nome="Ana" />
+          <FamiliaMembro nome="Gustavo" />
+
+        </Familia>
+      </Card>
+
+      <Card titulo="#04 - Desafio Aleatorio" color="#FA6900">
+        <Aleatorio min={1} max={60} />
+      </Card>
+
+      <Card titulo="#03 - Fragmento" color="#E94C6F">
         <Fragmento />
-        <ComParametro
-            titulo="Situação do Aluno"
-            aluno="Pedro"
-            nota={9.3}
-        />
+      </Card>
+
+      <Card titulo="#02 - Com Parâmetro" color="#E8B71A">
+        <ComParametro titulo="Situação do Aluno" aluno="Pedro" nota={9.3} />
+      </Card>
+
+      <Card titulo="#01 - Primeiro Componente" color="#588C73">
         <Primeiro></Primeiro>
-
+      </Card>
     </div>
+  </div>
 );
-

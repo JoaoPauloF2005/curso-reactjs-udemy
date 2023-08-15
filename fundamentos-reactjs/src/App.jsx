7680b2d730584a5f169b7037adeb94247c1cc74a
joao <hypergamerfrr@gmail.com>
2023-08-15 08:58:38 -0300

    att

--- fundamentos-reactjs/src/App.jsx
+++ fundamentos-reactjs/src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 import React from "react";
 
+import UsuarioInfo from "./components/condicional/UsuarioInfo";
+import ParOuImpar from "./components/condicional/ParOuImpar";
+import TabelaProdutos from "./components/repeticao/TabelaProdutos";
 import FamiliaMembro from "./components/basicos/FamiliaMembro";
+import ListaAlunos from "./components/repeticao/ListaAlunos";
 import Familia from "./components/basicos/Familia";
 import Card from "./components/layout/Card";
 import Aleatorio from "./components/basicos/Aleatorio";
@@ -14,12 +18,28 @@ export default () => (
     <h1>Fundamentos do React</h1>
 
     <div className="Cards">
+      <Card titulo="#08 - Renderização Condicional" color="#982395">
+        <ParOuImpar numero={20}></ParOuImpar>
+        <UsuarioInfo usuario={{nome: 'João' }} />
+        <UsuarioInfo usuario={{email: 'jo@.com' }} />
+        <UsuarioInfo usuario={{}} />
+
+
+      </Card>
+
+      <Card titulo="#07 - Desafio Repetição" color="#3A9AD9">
+        <TabelaProdutos></TabelaProdutos>
+      </Card>
+
+      <Card titulo="#06 - Repetição" color="#FF4C65">
+        <ListaAlunos></ListaAlunos>
+      </Card>
+
       <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
         <Familia sobrenome="Silva">
           <FamiliaMembro nome="Pedro" />
           <FamiliaMembro nome="Ana" />
           <FamiliaMembro nome="Gustavo" />
-
         </Familia>
       </Card>
 
---
+++ fundamentos-reactjs/src/data/produtos.js
@@ -0,0 +1,10 @@
+export default [
+    {id: 1, nome: 'Chinelo', preco: 10},
+    {id: 2, nome: 'Calça', preco: 50},
+    {id: 3, nome: 'Bermuda', preco: 100},
+    {id: 4, nome: 'Camisa', preco: 80},
+    {id: 5, nome: 'Blusa', preco: 150},
+    {id: 6, nome: 'Sutiã', preco: 40},
+    {id: 7, nome: 'Cueca', preco: 30},
+    {id: 8, nome: 'Calcinha', preco: 43},
+]

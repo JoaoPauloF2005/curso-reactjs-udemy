cb3009cdd04c91ad1772adcd1528a36db051f0ff
joao <hypergamerfrr@gmail.com>
2023-08-18 13:54:08 -0300

    att

---
+++ react-router/src/components/layout/Content.jsx
@@ -0,0 +1,17 @@
+import "./Content.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom"; // Importe esses arquivos
+ 
+import Home from "../../views/examples/Home";
+import About from "../../views/examples/About";
+ 
+const Content = props => (
+  <main className="Content">
+    <Routes>
+      <Route path="/about" element={<About />} />
+      <Route path="/" exact element={<Home />} />
+    </Routes>
+  </main>
+);
+
+export default Content;
